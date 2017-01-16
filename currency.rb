ba5432bad86b04a617611de6234999981741d3dd
nnelli <nnelli@automatedinsights.com>
2017-01-16 15:52:22 -0500

    Added == method for Currency class, added test for == method

--- currency.rb
+++ currency.rb
@@ -1,6 +1,12 @@
 class Currency
+  attr_reader :amount, :code
+
   def initialize(amount:, code:)
     @amount = amount
     @code = code
+  end
+
+  def ==(b)
+    @amount == b.amount && @code == b.code
   end
 end
--- currency_test.rb
+++ currency_test.rb
@@ -6,4 +6,13 @@ class CurrencyTest < Minitest::Test
   def test_currency_creation
     Currency.new(amount: 100, code: 'USD')
   end
+
+  def test_equal_currencies
+    a = Currency.new(amount: 100, code: 'USD')
+    b = Currency.new(amount: 100, code: 'USD')
+    assert a == b
+
+    c = Currency.new(amount: 34, code: 'EUR')
+    refute a == c
+  end
 end
